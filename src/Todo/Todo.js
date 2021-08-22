3a038cc70e3ec45faa5e3da1b41a9edc71388e5b
Michael Baliasny <Michael.Baliasny@gmail.com>
2021-08-22 10:29:24 +0300

    mmm

--- src/Todo/Todo.js
+++ src/Todo/Todo.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Todo.css';
 
 function Todo() {
+	const [value, setValue] = useState('');
+	const [tasks, setTasks] = useState([]);
+
+	const addTask = (task) => {
+
+		setTasks([...tasks,task]);
+		setValue('');
+	}
+	const onDelete = (index) =>{
+		setTasks(tasks.filter((t,i)=> i!==index));
+	}
 	return (
 		<div className="Todo">
 			<h3>Todo List:</h3>
 			<p>Make a Todo list (add & delete options):</p>
-			<input placeholder="Your task..." /> <button>Add task</button>
+			<input placeholder="Your task..." value={value}  onChange={e=>setValue(e.target.value)}/> <button onClick={()=>addTask(value)}>Add task</button>
+			<ul>
+				{tasks.length ? tasks.map((task,i) => <li key ={i}>{task} <button onClick={()=> onDelete(i)}>delete</button></li>) : ''}
+			</ul>
 		</div>
 	)
 }
